ae1174e3c746e8fb699413eb2df2b05b3229a888
MyltsinVV <vova.myltsin@yandex.ru>
2016-10-20 22:04:34 +0400

    Fix v1.0.1

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,18 @@
 abstract class Main extends Unit{
 
     public static void main(String[] args){
+
         Swordman swordman = new Swordman();
         Archer archer = new Archer();
+
         archer.attack(swordman);
+        System.out.println(swordman.currentHP);
+
         for (int i=0; i<10; i++) {
             swordman.attack(archer);
             if (archer.currentHP<0) break;
             System.out.println(archer.currentHP);
         }
-        System.out.print(swordman.currentHP);
     }
 
 }

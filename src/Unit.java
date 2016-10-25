10be2e3395d79c119aade32edee57048be7923a9
andrew-a-gusev <andrew.a.gusev@gmail.com>
2016-10-25 13:29:39 +0300

    Update Unit.java

--- src/Unit.java
+++ src/Unit.java
@@ -3,5 +3,9 @@ public abstract class Unit {
     int currentHP;
     int maxHP;
     int damage;
+    /**
+     * create - в сущности выполняет роль конструктора, 
+     * те можно было создать базовый конструктор (maxHP, damage) и вызывать его в конструкторах наследников
+     */
     public abstract void creat();
 }
